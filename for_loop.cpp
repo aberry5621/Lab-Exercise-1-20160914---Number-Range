d47a2b029d0be6cd47a6b0e4b7660b56c72fbf36
aberry5621 <aberry5621@mycom.marin.edu>
2016-09-14 20:00:24 -0700

    Create for_loop.cpp

---
+++ for_loop.cpp
@@ -0,0 +1,26 @@
+//
+//  for_loop.cpp
+//  Lab Exercise 3 20160914
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "For Loop \n";
+
+
+
+
+	// I // P  // O
+	for (int i = 10; i < 100; i = i+20) {
+
+		cout << i << endl;
+	}
+
+	// 0
+
+	return 0;
+}
