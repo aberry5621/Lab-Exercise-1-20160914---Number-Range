906d24d2e231becff72f6850b3c1b05b9400d900
aberry5621 <aberry5621@mycom.marin.edu>
2016-09-14 19:56:23 -0700

    Create while_loop.cpp

---
+++ while_loop.cpp
@@ -0,0 +1,34 @@
+//
+//  while_loop.cpp
+//  Lab Exercise 2 20160914
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "While Loop Programmed \n";
+
+	// Data and Initialization
+	int counter = 10;
+	int doubled = 0;
+
+	// I
+	cout << "Enter lower bound: ";
+
+
+	// P  // O
+	while (counter < 100) 
+	{
+		doubled = counter * 2;
+		cout << doubled << endl;
+		counter = counter + 10;
+	}
+
+	// 0
+
+
+	return 0;
+}
